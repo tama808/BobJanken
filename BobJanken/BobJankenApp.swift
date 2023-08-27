f8bbc897ca1f69408a447658447843e0deb5ab44
talow808 <808bobbrand@gmail.com>
2023-08-27 19:42:33 +0900

    Initial Commit

---
+++ BobJanken/BobJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BobJankenApp.swift
+//  BobJanken
+//
+//  Created by taro tama on 2023/08/27.
+//
+
+import SwiftUI
+
+@main
+struct BobJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
