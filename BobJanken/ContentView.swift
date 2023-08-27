c94dc5df49086d0abd5635ffc92c2d3156c32a6b
talow808 <808bobbrand@gmail.com>
2023-08-27 20:34:13 +0900

    first

--- BobJanken/ContentView.swift
+++ BobJanken/ContentView.swift
@@ -8,19 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var str = "Hello,SwiftUI"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Rectangle()
+                .foregroundColor(.gray)
+                .frame(width: 350, height: 100)
+            Rectangle()
+                .foregroundColor(.purple)
+                .frame(width: 350, height: 100)
+            HStack {
+                Rectangle()
+                    .foregroundColor(.blue)
+                    .frame(width: 150, height: 150)
+                Rectangle()
+                    .foregroundColor(.red)
+                    .frame(width: 100, height: 100)
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(width: 50, height: 50)
+            }
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.green)
+                    .frame(width: 350, height: 200)
+                HStack{
+                    VStack{
+                        Rectangle()
+                            .foregroundColor(.pink)
+                            .frame(width: 150, height: 70)
+                        Rectangle()
+                            .foregroundColor(.black)
+                            .frame(width: 150, height: 70)
+                    }
+                    VStack{
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .frame(width: 150, height: 70)
+                        Rectangle()
+                            .foregroundColor(.yellow)
+                            .frame(width: 150, height: 70)
+                    }
+                }
+            }
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
     }
 }
