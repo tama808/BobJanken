686db703adb7fe4516ee108342d469bbc5362c19
talow808 <808bobbrand@gmail.com>
2023-08-29 17:54:09 +0900

    ボタンカラー変更

--- BobJanken/PlayView.swift
+++ BobJanken/PlayView.swift
@@ -14,7 +14,9 @@ struct PlayView: View {
     @State private var textColor: Color = Color.black
     @State private var consecutiveWins: Int = 0
     @State private var lastResult: String = ""
-    @State private var isPlaying: Bool = false // 追加
+    @State private var isPlaying: Bool = false
+    @State private var buttonText: String = ""
+    @State private var buttonColor: Color = .blue
     
     var body: some View {
         VStack {
@@ -27,15 +29,14 @@ struct PlayView: View {
                     Image(enemyHand.imageName)
                         .resizable()
                         .frame(width: 60, height: 60)
-                        .padding(.bottom, 20) // スペースを確保
+                        .padding(.bottom, 20)
                 } else {
                     Image(enemyHand.imageName)
                         .resizable()
                         .frame(width: 60, height: 60)
                     Text("相手の手: \(enemyHand.description)")
-                        .padding(.top, 20) // スペースを確保
+                        .padding(.top, 20)
                 }
-                // 連勝を視覚的に表示するメーター
                 VerticalRainbowRectangleMeter(consecutiveWins: consecutiveWins)
             }
             
@@ -45,21 +46,21 @@ struct PlayView: View {
                         .resizable()
                         .frame(width: 60, height: 60)
                 }
-                .disabled(isPlaying) // isPlaying が true の場合はボタンを無効化
+                .disabled(isPlaying)
                 
                 Button(action: { self.play(hand: .scissors) }) {
                     Image(Hand.scissors.imageName)
                         .resizable()
                         .frame(width: 60, height: 60)
                 }
-                .disabled(isPlaying) // isPlaying が true の場合はボタンを無効化
+                .disabled(isPlaying)
                 
                 Button(action: { self.play(hand: .paper) }) {
                     Image(Hand.paper.imageName)
                         .resizable()
                         .frame(width: 60, height: 60)
                 }
-                .disabled(isPlaying) // ボタンを無効化
+                .disabled(isPlaying)
             }
             
             Text(resultMessage)
@@ -67,14 +68,22 @@ struct PlayView: View {
                 .foregroundColor(textColor)
                 .padding()
             
-            Button("再挑戦", action: {
-                self.stopRandomTimer() // タイマーを停止
-                self.resultMessage = ""
-                self.enemyHand = .rock // 相手の手を初期化
-                self.isRandomlyDisplaying = true // ランダム表示を再開
-                self.startRandomTimer() // タイマーを再開
-                self.isPlaying = true // 試合中フラグを設定
-            })
+            // 初回のプレイ結果時にのみ表示
+            if isPlaying {
+                Button(buttonText, action: {
+                    self.stopRandomTimer()
+                    self.resultMessage = ""
+                    self.enemyHand = .rock
+                    self.isRandomlyDisplaying = true
+                    self.startRandomTimer()
+                    self.isPlaying = true
+                })
+                .padding() // ボタンのパディング
+                .background(buttonColor) // ボタンの背景色
+                .foregroundColor(.white) // ボタンのテキスト色
+                .cornerRadius(10) // 角丸
+                .font(.headline) // ボタンのフォント
+            }
             
             Spacer()
         }
@@ -85,6 +94,8 @@ struct PlayView: View {
             self.stopRandomTimer()
         }
     }
+    
+    
     
     private func play(hand: Hand) {
         if isPlaying {
@@ -124,18 +135,25 @@ struct PlayView: View {
         case (.rock, .scissors), (.scissors, .paper), (.paper, .rock):
             resultMessage = "勝ち！"
             textColor = Color.red
+            buttonText = "連勝に挑戦"
+            buttonColor = Color.red
             consecutiveWins += 1 // 連勝回数をインクリメント
             lastResult = "win" // 前回の結果を保持
         case (.rock, .paper), (.scissors, .rock), (.paper, .scissors):
             resultMessage = "負け！"
             textColor = Color.blue
+            buttonText = "再挑戦だー"
+            buttonColor = Color.blue
             consecutiveWins = 0 // 連勝回数をリセット
             lastResult = "lose" // 前回の結果を保持
         default:
             resultMessage = "引き分け！"
             textColor = Color.black
+            buttonText = "あいこはもう一回"
+            buttonColor = Color.green
             // 引き分けの場合は連勝回数をリセットしない
             lastResult = "draw" // 前回の結果を保持
+            
         }
     }
     
@@ -202,5 +220,6 @@ struct PlayView: View {
             return min(height, maxBarHeight)
         }
     }
+    
     
 }
