c10e4bf993a29786f641bfe912adaf0a23a57273
talow808 <808bobbrand@gmail.com>
2023-08-31 16:36:06 +0900

    トップデザインたたき

--- BobJanken/ContentView.swift
+++ BobJanken/ContentView.swift
@@ -18,86 +18,102 @@ struct ContentView: View {
     @State var isPlayViewPresented = false
     @State var isSignUpViewPresented = false
     @AppStorage("savedNickname") private var savedNickname = ""
-    
+  
     var body: some View {
-        ZStack(alignment: .center) {
-            Color.gray
-            
-            VStack {
-                if !savedNickname.isEmpty {
-                    Text("ようこそ、\(savedNickname) さん！")
-                        .font(.title)
-                        .foregroundColor(.white)
-                        .padding(.top, 20)
-                }
+        NavigationView {
+            ZStack(alignment: .center) {
+                Image("main_back")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
                 
-                Text("ボブくんの\n気ままにじゃんけん")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .padding(.vertical, 30)
-                
-                if savedNickname.isEmpty {
-                    Button(action: {
-                        isSignUpViewPresented = true
-                    }) {
-                        Text("登録する")
-                            .foregroundColor(.white)
-                            .font(.headline)
-                            .frame(width: 150, height: 60)
-                            .background(Color.pink)
-                            .cornerRadius(10)
-                    }
-                    .fullScreenCover(isPresented: $isSignUpViewPresented) {
-                        SignUpView()
-                    }
-                    .padding(.bottom, 20)
-                } else {
-                    Button(action: {
-                        isPlayViewPresented = true
-                    }) {
-                        Text("勝負する")
-                            .foregroundColor(.white)
-                            .font(.headline)
-                            .frame(width: 300, height: 100)
-                            .background(Color.blue)
-                            .cornerRadius(10)
-                    }
-                    .fullScreenCover(isPresented: $isPlayViewPresented) {
-                        PlayView()
-                    }
-                    .padding(.bottom, 20)
+                VStack {
+                    Spacer()
                     
-                    Button(action: {
-                        savedNickname = ""
-                    }) {
-                        Text("ログアウト")
-                            .foregroundColor(.white)
-                            .font(.headline)
-                            .frame(width: 100, height: 40)
-                            .background(Color.red)
-                            .cornerRadius(10)
+                    Text("ボブくんの\n気ままにじゃんけん")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                        .padding(.vertical, 30)
+                    
+                    if savedNickname.isEmpty {
+                        Button(action: {
+                            isSignUpViewPresented = true
+                        }) {
+                            Text("登録する")
+                                .foregroundColor(.white)
+                                .font(.headline)
+                                .frame(width: 150, height: 60)
+                                .background(Color.pink)
+                                .cornerRadius(10)
+                        }
+                        .fullScreenCover(isPresented: $isSignUpViewPresented) {
+                            SignUpView()
+                        }
+                        .padding(.bottom, 20)
+                    } else {
+                        Button(action: {
+                            isPlayViewPresented = true
+                        }) {
+                            Text("勝負する")
+                                .foregroundColor(.white)
+                                .font(.headline)
+                                .frame(width: 300, height: 100)
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        }
+                        .fullScreenCover(isPresented: $isPlayViewPresented) {
+                            PlayView()
+                        }
+                        .padding(.bottom, 20)
+                        
+                        Button(action: {
+                            savedNickname = ""
+                        }) {
+                            Text("ログアウト")
+                                .foregroundColor(.white)
+                                .font(.headline)
+                                .frame(width: 100, height: 40)
+                                .background(Color.red)
+                                .cornerRadius(10)
+                        }
+                        
+                        if !savedNickname.isEmpty {
+                            HStack {
+                                ZStack(alignment: .topLeading) {
+                                    Image("fuki")
+                                        .resizable()
+                                        .frame(width: 250, height: 150)
+                                        .offset(x: -30, y: 0)
+                                    
+                                   
+                                    Text("ようこそ、\n\(savedNickname) さん！\n楽しんでくださいね\n今日は負けないぞ")
+                                        .font(.system(size: 14))
+                                        .foregroundColor(.black)
+                                        .padding(15)
+                                        .offset(x: -10, y: 30)
+                                    Spacer()
+                            Image("b-main")
+                                .resizable()
+                                .frame(width: 300, height: 300)
+                                .aspectRatio(contentMode: .fit)
+                                .offset(x: 70,y: 25)
+                        }
+                            }
+                        }
+                        
+                        HStack {
+                            Image("b-nozoki")
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                                .aspectRatio(contentMode: .fit)
+                        }
+                        .padding(.horizontal, 20)
                     }
-                    
-                    Spacer() // スペースを追加して画像を上に押し上げる
-                    
-                    Image("b-main")
-                        .resizable()
-                        .frame(width: 300, height: 300)
-                        .aspectRatio(contentMode: .fit)
-                        .offset(y: 25) // 画像を少し上に移動
-            
                 }
+                .navigationBarHidden(true)
+                .edgesIgnoringSafeArea(.all)
             }
-            .navigationBarHidden(true)
-            .edgesIgnoringSafeArea(.all)
         }
-        HStack {
-            Image("b-nozoki")
-                .resizable()
-                .frame(width: 100, height: 100)
-                .aspectRatio(contentMode: .fit)
-        }
-        .padding(.horizontal, 20)
     }
 }
