52af1ca6d1f23d7a28d2ae4ae7e450c78c3362b6
talow808 <808bobbrand@gmail.com>
2023-09-15 16:59:29 +0900

    修正

--- BobJanken/PlayView.swift
+++ BobJanken/PlayView.swift
@@ -11,40 +11,33 @@
 //  Created by taro tama on 2023/08/28.
 
 import SwiftUI
-import AVFoundation
 
 struct PlayView: View {
     
-    // プロパティ定義
     @State private var score = 0
-    @State private var audioPlayer: AVAudioPlayer?
-    @State var numberA: Int = Int.random(in: 2...9)
-    @State var numberB: Int = 0
-    @State var prediction: Int = 0
-    @State var result: String = ""
-    @State var showNumberB: Bool = false
-    @State var gameStatus: GameStatus? = nil
-    @State var isGameFinished: Bool = false
-    @State var isButtonsEnabled: Bool = true
+    @State private var highScore = UserDefaults.standard.integer(forKey: "highestScore")
+    @State private var numberA: Int = Int.random(in: 2...9)
+    @State private var numberB: Int = 0
+    @State private var prediction: Int = 0
+    @State private var result: String = ""
+    @State private var showNumberB: Bool = false
+    @State private var gameStatus: GameStatus? = nil
+    @State private var isGameFinished: Bool = false
+    @State private var isButtonsEnabled: Bool = true
     @AppStorage("score") private var savedScore = 0
-    @State var isRankingViewPresented = false
-    var userID: String = "" // ユーザーIDを追加
+    @State private var isRankingViewPresented = false
+    var userID: String = ""
+    
     enum GameStatus {
         case win
         case lose
     }
     let highestScoreKey = "highestScore"
-    let ScoreDigits: [String]
     
-    init() {
-        let score = UserDefaults.standard.integer(forKey: highestScoreKey)
-        self._score = State(initialValue: score)
-        self.ScoreDigits = String(score).map { String($0) }
-    }
     var body: some View {
         
         ZStack(alignment: .bottom) {
-            SwiftUI.Image("main_back")
+            Image("main_back")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .edgesIgnoringSafeArea(.all)
@@ -62,23 +55,18 @@ struct PlayView: View {
                         VStack {
                             HStack(spacing: -5)  {
                                 // スコア表示
-                                ForEach(ScoreDigits, id: \.self) { digit in
-                                    Image("simage\(digit)")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 20, height: 30)
-                                        .offset(x: 70,y: -10)
-                                }
+                                Text("\(score)")
+                                    .font(.system(size: 24, weight: .bold))
+                                    .frame(width: 100, height: 50) // 幅と高さを設定
+                                    .offset(x: 80, y: 0) // X軸方向に50ポイント右にずらす
                             }
                             HStack(spacing: -5) {
                                 // ベストスコア表示
-                                ForEach(ScoreDigits, id: \.self) { digit in
-                                    Image("simage\(digit)")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 20, height: 30)
-                                        .offset(x: 70,y: 17)
-                                }
+                                Text("\(highScore)")
+                                    .font(.system(size: 24, weight: .bold))
+                                    .frame(width: 100, height: 50) // 幅と高さを設定
+                                    .offset(x: 80, y: 0) // X軸方向に50ポイント右にずらす
+                                
                             }
                         }
                     }
@@ -99,27 +87,24 @@ struct PlayView: View {
                     Spacer()
                 }
                 Spacer()
-              
-                
-             
-                ZStack {
+            ZStack {
                     Image("playback")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 400, height: 400)
                     
                     if gameStatus == .win {
-                        Image("win") // "winImage"は実際の勝利画像の名前に置き換えてください
+                        Image("win")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(width: 200, height: 200) // 画像サイズを調整
-                            .offset(x: 50, y: -100) // "playback"の右上に配置
+                            .frame(width: 200, height: 200)
+                            .offset(x: 50, y: -100)
                     } else if gameStatus == .lose {
-                        Image("lose") // "loseImage"は実際の敗北画像の名前に置き換えてください
+                        Image("lose")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(width: 200, height: 200) // 画像サイズを調整
-                            .offset(x: 50, y: -100) // "playback"の右上に配置
+                            .frame(width: 200, height: 200)
+                            .offset(x: 50, y: -100)
                     }
                     
                     if showNumberB {
@@ -138,47 +123,35 @@ struct PlayView: View {
                         .padding(.trailing, 200)
                     }
                 }
-
+                
                 Spacer()
                 // ハイボタンとローボタンを横並びに配置
                 HStack {
                     // ハイボタン
                     Button(action: {
                         onHighButtonTap()
-                        audioPlayer?.play()
                     }) {
-                        SwiftUI.Image("up") // 画像オブジェクトを渡す
-                            .resizable() // 画像をリサイズ可能にする
-                            .frame(width: 80, height: 90) // 画像の幅と高さを設定
+                        Image("up")
+                            .resizable()
+                            .frame(width: 80, height: 90)
                     }
                     .padding()
                     Image("image\(numberA)")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit) // 画像のアスペクト比を保持してフィットさせる
-                    .frame(width: 80, height: 90)
-                        .padding(.bottom, 20) // 下部の余白を追加
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80, height: 90)
+                        .padding(.bottom, 20)
                     // ローボタン
                     Button(action: {
                         onLowButtonTap()
-                        audioPlayer?.play()
                     }) {
-                        SwiftUI.Image("down") // 画像オブジェクトを渡す
-                            .resizable() // 画像をリサイズ可能にする
-                            .frame(width: 80, height: 90) // 画像の幅と高さを設定
+                        Image("down")
+                            .resizable()
+                            .frame(width: 80, height: 90)
                     }
-                    .padding() // ボタン間の余白を追加
+                    .padding()
                 }
-                .onAppear {
-                    if let soundURL = Bundle.main.url(forResource: "drumroll", withExtension: "mp3") {
-                        do {
-                            audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
-                            audioPlayer?.prepareToPlay()
-                        } catch {
-                            print("Error loading sound file: \(error.localizedDescription)")
-                        }
-                    }
-                }
-                Spacer()
+                
                 
                 // ゲームステータスに応じたボタン
                 switch gameStatus {
@@ -195,25 +168,22 @@ struct PlayView: View {
                     .buttonStyle(CustomButtonStyle2())
                 default:
                     EmptyView()
+                    Spacer()
                 }
-                
             }
-            .padding(20) // 余白を追加
+            .padding(20)
             .onAppear {
                 // 初期設定
                 setupInitialGame()
             }
         }
     }
+    
     // ハイボタンがタップされた時の処理
     private func onHighButtonTap() {
-        // ハイボタンを押したら、予測をハイに設定する
         prediction = 1
-        
-        // タイマーを停止する
         isGameFinished = true
         
-        // 結果を計算して表示し、数字Bも表示する
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             result = predictResult()
             showNumberB = true
@@ -223,20 +193,15 @@ struct PlayView: View {
             } else {
                 gameStatus = .lose
             }
-            // スコアをユーザーデフォルトに保存
-            UserDefaults.standard.set(score, forKey: "score")
+            saveScore()
         }
     }
     
     // ローボタンがタップされた時の処理
     private func onLowButtonTap() {
-        // ローボタンを押したら、予測をローに設定する
         prediction = -1
-        
-        // タイマーを停止する
         isGameFinished = true
         
-        // 結果を計算して表示し、数字Bも表示する
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             result = predictResult()
             showNumberB = true
@@ -245,13 +210,13 @@ struct PlayView: View {
                 gameStatus = .win
             } else {
                 gameStatus = .lose
+                saveScore()
             }
         }
     }
     
     // 次の試合ボタンがタップされた時の処理
     private func onNextGameButtonTap() {
-        // 次の試合ボタンを押したら、新しい数字Aとリセットする
         numberA = Int.random(in: 2...9)
         repeat {
             numberB = Int.random(in: 1...10)
@@ -261,13 +226,11 @@ struct PlayView: View {
     
     // 再挑戦ボタンがタップされた時の処理
     private func onRetryButtonTap() {
-        // 再挑戦ボタンを押したら、数字Aと数字Bをリセットし、結果とタイマーとスコアをクリアする
         numberA = Int.random(in: 2...9)
         repeat {
             numberB = Int.random(in: 1...10)
         } while numberB == numberA
         resetGame()
-        // スコアを0にリセット
         score = 0
     }
     
@@ -301,31 +264,27 @@ struct PlayView: View {
     // スコアの更新
     private func updateScore() {
         if result == "勝ち" {
+            var scoreIncrement = 1
+            
             if (numberA == 4 && prediction == -1) || (numberA == 7 && prediction == 1) {
-                score += 2
+                scoreIncrement = 2
             } else if (numberA == 3 && prediction == -1) || (numberA == 8 && prediction == 1) {
-                score += 3
+                scoreIncrement = 3
             } else if (numberA == 2 && prediction == -1) || (numberA == 9 && prediction == 1) {
-                score += 5
-            } else {
-                score += 1
+                scoreIncrement = 5
             }
-            if score > getHighestScore() {
-                UserDefaults.standard.set(score, forKey: highestScoreKey)
-            }
+            
+            score += scoreIncrement
+            savedScore = score
         }
     }
     
-    // 最高スコアの取得
-    private func getHighestScore() -> Int {
-        return UserDefaults.standard.integer(forKey: highestScoreKey)
-    }
-    
     // スコアの保存
+
     private func saveScore() {
-        if score > savedScore {
-            savedScore = score
-            UserDefaults.standard.set(savedScore, forKey: highestScoreKey)
+        if score > highScore {
+            highScore = score
+            UserDefaults.standard.set(highScore, forKey: highestScoreKey)
         }
     }
     
@@ -333,9 +292,9 @@ struct PlayView: View {
         func makeBody(configuration: Configuration) -> some View {
             configuration.label
                 .padding()
-                .background(Color.red) // ボタンの背景色
-                .foregroundColor(.white) // ボタンのテキストカラー
-                .cornerRadius(10) // ボタンの角丸
+                .background(Color.red)
+                .foregroundColor(.white)
+                .cornerRadius(10)
         }
     }
     
@@ -343,9 +302,9 @@ struct PlayView: View {
         func makeBody(configuration: Configuration) -> some View {
             configuration.label
                 .padding()
-                .background(Color.orange) // ボタンの背景色
-                .foregroundColor(.white) // ボタンのテキストカラー
-                .cornerRadius(10) // ボタンの角丸
+                .background(Color.orange)
+                .foregroundColor(.white)
+                .cornerRadius(10)
         }
     }
 }
